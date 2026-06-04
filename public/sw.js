373b1cfd4c7d2dc233ebaf3c3ec3cf57afa7aea3
Tony Lampel <me@tony-marc.de>
2026-06-04 19:27:50 +0200

    build 2.3.3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'miolog-v2.3.2'
+const CACHE_VERSION = 'miolog-v2.3.3'
 const SHELL_CACHE = `${CACHE_VERSION}-shell`
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`
 const APP_SHELL_URLS = [
